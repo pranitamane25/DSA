bf06c93224dc30693765dfa34b9a8d58b8e53651
pranitamane25 <pranitamane2506@gmail.com>
2026-07-01 17:20:49 +0530

    Basic Linkedlist program

---
+++ Linked_List/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Node
+{
+    public int Data;
+    public Node Next;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Node n1 = new Node();
+        n1.Data = 10;
+
+        Node n2 = new Node();
+        n2.Data = 20;
+
+        Node n3 = new Node();
+        n3.Data = 30;
+
+        n1.Next = n2;
+        n2.Next = n3;
+        n3.Next = null;
+
+        Node temp = n1;
+
+        while (temp != null)
+        {
+            Console.WriteLine(temp.Data);
+            temp = temp.Next;
+        }
+    }
+}
