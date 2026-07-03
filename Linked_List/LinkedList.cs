c4b86d62d2cce9817f6210cbdc9a2372daf49ed2
pranitamane25 <pranitamane2506@gmail.com>
2026-07-03 16:58:42 +0530

    add first and add last added

--- Linked_List/LinkedList.cs
+++ Linked_List/LinkedList.cs
@@ -107,5 +107,30 @@ public class LinkedList
             Console.WriteLine("Node Not Found");
         }
 
+        public void InsertInBetween(int data)
+            {
+                Node newNode = new Node(data);
+
+                // If the list is empty or the new node should be the first node
+                if (head == null || head.Data >= data)
+                {
+                    newNode.Next = head;
+                    head = newNode;
+                    return;
+                }
+
+                Node current = head;
+
+                // Traverse until the correct position is found
+                while (current.Next != null && current.Next.Data < data)
+                {
+                    current = current.Next;
+                }
+
+                // Insert the new node
+                newNode.Next = current.Next;
+                current.Next = newNode;
+            }
+
             
 }
