e3decdb54d97233fcf79d86d05fee20b9c943053
pranitamane25 <pranitamane2506@gmail.com>
2026-07-04 11:06:31 +0530

    retrive data from array using for and foreach loop

---
+++ Array_retrieve/Program.cs
@@ -0,0 +1,20 @@
+﻿//retrieve data from array
+
+int[]array={10,20,30,40,50};
+
+//using foreach loop
+
+// foreach(int arr1 in array)
+// {
+//     Console.WriteLine(arr1);
+// }
+
+//using for loop
+
+for(int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
+
+
+
