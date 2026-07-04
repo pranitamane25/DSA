0ac84faf6d49bc4bb7cc7edab297a5e9f052fd0f
pranitamane25 <pranitamane2506@gmail.com>
2026-07-04 12:02:44 +0530

    find largest element from array

---
+++ largest_element_fromArray/Program.cs
@@ -0,0 +1,16 @@
+﻿
+//Find largest element from array
+
+int [] arr={10,29,30,40,80};
+
+int largest=arr[0];
+
+for(int i = 1; i < arr.Length; i++)
+{
+    if (arr[i] > largest)
+    {
+        largest=arr[i];
+    }
+}
+
+Console.WriteLine("Largest Element :" + largest);
