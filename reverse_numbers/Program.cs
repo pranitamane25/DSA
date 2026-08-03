a79cca60d1b40dc7f3b475c011606cd1461e8c52
pranitamane25 <pranitamane2506@gmail.com>
2026-08-03 13:21:01 +0530

    reverse the given numbers

---
+++ reverse_numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿//reverse numbers
+public class Program{
+    public static void Main(){
+
+        int num=12345;
+        int reverse=0;
+
+        while (num > 0)
+        {
+            int digit =num%10;
+            reverse=reverse*10+digit;
+            num=num/10;
+        }
+        Console.WriteLine(reverse);
+
+    }
+}
