ce50afeb7b072edb4e474a728a6b985748b9bff1
pranitamane25 <pranitamane2506@gmail.com>
2026-08-03 12:51:24 +0530

    reverse string problem

---
+++ Reverse_string/Program.cs
@@ -0,0 +1,19 @@
+﻿
+//reverse string 
+
+
+public class Program{
+    public static void Main()
+    {
+        string str="Pranita";
+
+        string reverse="";
+
+        for(int i = str.Length - 1; i >= 0; i--)
+        {
+            reverse+=str[i];
+        }
+        Console.WriteLine(reverse);
+
+    }
+}
