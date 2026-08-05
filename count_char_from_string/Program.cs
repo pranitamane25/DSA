8ce43b3cfda89d65833531dc2f6307966f7e7310
pranitamane25 <pranitamane2506@gmail.com>
2026-08-05 13:13:30 +0530

    count the character from string

---
+++ count_char_from_string/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection.PortableExecutable;
+
+public class Program{
+    public static void Main(){
+
+        string str="Hello pranita";
+        int count=0;
+
+        for(int i = 0; i < str.Length; i++)
+        {
+            count++;
+        }
+        Console.WriteLine("Characters count : " + count);
+    }
+}
