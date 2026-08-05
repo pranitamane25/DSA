12466e2e3961f810058eeae3000aa9f37591c1c4
pranitamane25 <pranitamane2506@gmail.com>
2026-08-05 12:52:05 +0530

    count the number of digits

---
+++ count_of_digits/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program{
+    public static void Main(){
+        int num=1234578903;
+        int count=0;
+
+        while (num > 0)
+        {
+            count++;
+            num=num/10;
+        }
+        Console.WriteLine(count);
+    }
+}
