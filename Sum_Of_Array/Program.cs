8d114347410cc10a6a17b4b7cc6b7697d1df6d2f
pranitamane25 <pranitamane2506@gmail.com>
2026-08-11 13:19:25 +0530

    sum of array

---
+++ Sum_Of_Array/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        int[] arr={10,20,40,50};
+        int sum=0;
+
+        for(int i = 0; i < arr.Length; i++)
+        {
+            sum=sum+arr[i];
+        }
+        Console.WriteLine("Sum of Array :" +sum );
+    }
+}
