f9f9d6f0f5285790050d334becb9123b8f7573b7
pranitamane25 <pranitamane2506@gmail.com>
2026-08-11 13:07:02 +0530

    find smallest element from  array

---
+++ Smallest_element_FromArray/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program{
+    public static void Main()
+    {
+        int[] arr={10,30,40,2,50};
+        int smallest=arr[0];
+
+        for(int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] < smallest)
+            {
+                smallest=arr[i];
+            }          
+        }
+    Console.WriteLine("Smallest Element From array :" +smallest );
+    }  
+}
