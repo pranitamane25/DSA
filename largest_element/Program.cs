b13466ac175e93b5d4eabf58002b64ecc6befc03
pranitamane25 <pranitamane2506@gmail.com>
2026-08-11 12:41:15 +0530

    largest element from array

---
+++ largest_element/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program{
+    public static void Main()
+    {
+        
+        int[]arr={10,20,49,50,70};
+        int largest=arr[0];
+
+        for(int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > largest)
+            {
+                largest=arr[i];
+            }
+
+        }
+        Console.WriteLine(largest);
+        
+    }
+}
