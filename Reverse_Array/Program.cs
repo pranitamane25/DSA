ac03c22b2a0a843735b07c265cabb9ad46999fca
pranitamane25 <pranitamane2506@gmail.com>
2026-08-11 15:10:40 +0530

    reverse elements from array

---
+++ Reverse_Array/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program{
+    public static void Main(){
+
+        int[]arr={1,2,3,4,5,6,7,8};
+
+        for(int i = arr.Length - 1; i >= 0; i--)
+        {
+            Console.Write(arr[i]);
+        }
+    }
+}
