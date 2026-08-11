dcf62ccac4fb45261eef8ff7ff15e5f21da69efb
pranitamane25 <pranitamane2506@gmail.com>
2026-08-11 13:33:00 +0530

    count even and odd elements

---
+++ count_EvenAndOdd_Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program{
+    public static void Main()
+    {
+        int even=0;
+        int odd=0;
+        int num=0;
+        
+
+        int[] arr={10,20,31,89,80};
+
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i]%2==0){
+            even++;
+            }
+            else
+            {
+                odd++;
+            }
+            
+        }
+        Console.WriteLine("even Numbers:" +even);
+        Console.WriteLine("Odd numbers:" +odd);
+
+    }
+}
